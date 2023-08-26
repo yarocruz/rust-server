98990c4e0b75eeca6739f8e5ec9bb08bb8e69e29
yarocruz <yarocruz@gmail.com>
2023-08-26 11:23:09 -0400

    handle and capture the connection stream

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
     // TcpListener.bind() creates a "listener" at the address and port indicated and returns a Result type
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -7,6 +11,23 @@ fn main() {
     for stream in listener.incoming() {
         // shadow the stream hear. we're not doing anything with the stream here for now
         let stream = stream.unwrap();
-        println!("Connection established");
+
+        handle_connection(stream);
     }
+}
+
+// function will handle the streams
+fn handle_connection(mut stream: TcpStream) {
+    /*
+        Every time a request is done we're going to read that stream into a buffer
+        and collect those reads/treams into a vector(growable array for the javascripties)
+     */
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+    .lines()
+    .map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())// I believe we need to use this to take ownership
+    .collect();
+
+    println!("Request: {:#?}", http_request);
 }

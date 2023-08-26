fb7593cdaea42c5115142775f2307b98ce72d5ce
yarocruz <yarocruz@gmail.com>
2023-08-26 10:46:55 -0400

    start the TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::net::TcpListener;
 fn main() {
-    println!("Hello, world!");
+    // TcpListener.bind() creates a "listener" at the address and port indicated and returns a Result type
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // for every incoming request to the listener create a stream
+    for stream in listener.incoming() {
+        // shadow the stream hear. we're not doing anything with the stream here for now
+        let stream = stream.unwrap();
+        println!("Connection established");
+    }
 }

a9964f9de512f0ffbf66213cba17ec3a04d29aae
yarocruz <yarocruz@gmail.com>
2023-08-27 13:40:44 -0400

    create Worker struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -15,13 +15,13 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            // create threads and store in vec
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
     pub fn execute<F>(&self, f: F)
     where
@@ -29,4 +29,16 @@ impl ThreadPool {
         {
 
         }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+   fn new(id: usize) -> Worker {
+    let thread = thread::spawn(|| {});
+    Worker { id, thread }
+   } 
 }
